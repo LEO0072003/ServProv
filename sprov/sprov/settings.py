a5272c909ee50aeec0d393a971599b8661082a42
LEO0072003 <106365961+LEO0072003@users.noreply.github.com>
2022-10-22 21:18:32 +0530

    Deploy

--- sprov/sprov/settings.py
+++ sprov/sprov/settings.py
@@ -120,9 +120,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -130,3 +133,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+'''For heroku Deployment'''
+# import django_heroku
+# django_heroku.settings(locals())
